66e9d7468e9676dac6ab9fee893ee3a20cdcd0c1
robosystem <75792010+robosystem@users.noreply.github.com>
2025-01-19 00:39:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_echarts import st_echarts
 
 st.title("Strategie di trading")
-st.write("Qualsiasi investimento comporta un determinato grado di rischio, inclusa la possibile \
+st.write("Qualsiasi investimento comporta un grado di rischio, inclusa la possibile \
 perdita di capitale. Non vi è alcuna garanzia che gli obiettivi di investimento della strategia \
 saranno raggiunti. Il presente strumento non intende pertanto costituire una raccomandazione o un \
 consiglio di investimento, né costituisce una sollecitazione all’acquisto o alla vendita \
@@ -17,40 +17,39 @@ st.write("Investimento", volume)
 target = st.number_input("Target", min_value=0.0)
 if target > 0 and volume > 0:
 	st.write("Controvalore obiettivo", round(qta*target, 2))
-	increment = ((qta*target)-(qta*acquisto))/volume
+	incremento = ((qta*target)-(qta*acquisto))/volume
 	profitto = round((qta*target)-volume,2)
-	st.write("Guadagno atteso", f'{increment:.2%}'+" ("+str(profitto)+")")
+	st.write("Guadagno atteso", f'{incremento:.2%}'+" ("+str(profitto)+")")
 stop = st.number_input("Stop Loss", min_value=0.0)
 if stop > 0 and volume > 0:
 	st.write("Controvalore minimo", round(qta*stop, 2))
-	decrement = ((qta*stop)-(qta*acquisto))/volume
+	decremento = ((qta*stop)-(qta*acquisto))/volume
 	perdita = round((qta*stop)-volume,2)
-	st.write("Perdita attesa", f'{decrement:.2%}'+" ("+str(perdita)+")")
+	st.write("Perdita attesa", f'{decremento:.2%}'+" ("+str(perdita)+")")
 if target > 0 and stop > 0:
 	st.header("Strategia")
 	options = {
-		#"title": {"text": "Strategia"},
 		"toolbox": {"feature": {"saveAsImage": {}}},
 		"tooltip": {
 			"show": "true",
-		    "trigger": "axis",
-		    "triggerOn": "click",
+		    	"trigger": "axis",
+		    	"triggerOn": "click",
 		},
 		"legend": {"data": ["strumento", "take profit", "stop loss"]},
 		"grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
 		"xAxis": {
-		    "type": "category",
+		    	"type": "category",
 			"boundaryGap": False,
 			"data": ["Inizio", "Fine"],
 		},
 		"yAxis": {"type": "value"},
 		"series": [
 	        {
-	            "name": "strumento",
-	            "type": "line",
-				"smooth": "true",
-	            "stack": "strumento",
-	            "data": [acquisto, acquisto],
+	            	"name": "strumento",
+	            	"type": "line",
+			"smooth": "true",
+	            	"stack": "strumento",
+	            	"data": [acquisto, acquisto],
 				"itemStyle": {
 					"color": 'blue'
 				},
@@ -65,7 +64,7 @@ if target > 0 and stop > 0:
 	            "name": "take profit",
 				"label": {
 					"show": "true",
-					"formatter": f'{increment:.2%}',
+					"formatter": f'{incremento:.2%}',
 				},
 	            "type": "line",
 				"smooth": "true",
@@ -85,7 +84,7 @@ if target > 0 and stop > 0:
 	            "name": "stop loss",
 				"label": {
 					"show": "true",
-					"formatter": f'{decrement:.2%}',
+					"formatter": f'{decremento:.2%}',
 				},
 	            "type": "line",
 				"smooth": "true",
@@ -123,36 +122,35 @@ if cont:
 			st.write("Nuovo controvalore", round(volume+costo,2))
 
 			riepilogo = {
-				#"title": {"text": "Rimodulazione"},
 				"toolbox": {"feature": {"saveAsImage": {}}},
 				"tooltip": {
 					"show": "true",
-				    "trigger": "axis",
-				    "triggerOn": "click",
+				    	"trigger": "axis",
+				    	"triggerOn": "click",
 				},
 				"legend": {"data": ["posizione", "ipotesi", "take profit", "stop loss", "prezzo"]},
 				"grid": {"left": "3%", "right": "4%", "bottom": "3%", "containLabel": True},
 				"xAxis": {
-				    "type": "category",
+				    	"type": "category",
 					"boundaryGap": False,
 					"data": ["Inizio", "Ipotesi", "Fine"],
 				},
 				"yAxis": {"type": "value"},
 				"series": [
 					{
-						"name": "posizione",
-						"type": "line",
-						"smooth": "true",
-						"stack": "posizione",
-						"data": [acquisto, acquisto, 'null'],
-						"itemStyle": {
-							"color": 'blue'
+					"name": "posizione",
+					"type": "line",
+					"smooth": "true",
+					"stack": "posizione",
+					"data": [acquisto, acquisto, 'null'],
+					"itemStyle": {
+						"color": 'blue'
 						},
 						"lineStyle": {
-					        "normal": {
-					          "color": 'blue',
-					          "width": 4,
-					        }
+						        "normal": {
+						        "color": 'blue',
+							"width": 4,
+						 	}
 						}
 					},
 					{
@@ -165,18 +163,18 @@ if cont:
 							"color": 'orange'
 						},
 						"lineStyle": {
-					        "normal": {
-					          "color": 'orange',
-					          "width": 2,
-					        }
+						        "normal": {
+						          "color": 'orange',
+						          "width": 2,
+						        }
 						}
 					},
 			        {
-			            "name": "ipotesi",
-			            "type": "line",
-						"smooth": "true",
-			            "stack": "ipotesi",
-			            "data": ['null', new, new],
+			            	"name": "ipotesi",
+			            	"type": "line",
+					"smooth": "true",
+			            	"stack": "ipotesi",
+			            	"data": ['null', new, new],
 						"itemStyle": {
 							"color": 'blue',
 							"type": 'dashed'
@@ -189,58 +187,58 @@ if cont:
 						}
 			        },
 			        {
-			            "name": "take profit",
-			            "type": "line",
-						"smooth": "true",
-			            "stack": "take profit",
-			            "data": [target, target, target],
-						"itemStyle": {
-							"color": 'green'
+			            	"name": "take profit",
+			            	"type": "line",
+					"smooth": "true",
+			            	"stack": "take profit",
+			            	"data": [target, target, target],
+					"itemStyle": {
+						"color": 'green'
 						},
-						"lineStyle": {
+					"lineStyle": {
 					        "normal": {
-					          "color": 'green',
-					          "width": 2
-							}
+					        "color": 'green',
+					        "width": 2
 						}
+					}
 			        },
 			        {
-			            "name": "stop loss",
-			            "type": "line",
-						"smooth": "true",
-			            "stack": "stop loss",
-			            "data": [stop, stop, stop],
-						"itemStyle": {
-							"color": 'red'
+			            	"name": "stop loss",
+			            	"type": "line",
+					"smooth": "true",
+			            	"stack": "stop loss",
+			            	"data": [stop, stop, stop],
+					"itemStyle": {
+						"color": 'red'
 						},
-						"lineStyle": {
-					        "normal": {
-					          "color": 'red',
-					          "width": 2
-							}
+					"lineStyle": {
+				        "normal": {
+					        "color": 'red',
+						"width": 2
 						}
+					}
 			        },
 				],
 			}
 			st_echarts(options=riepilogo, height="500px")
 
 			#-195.96 (-21.49%)
-			hyp_increment = (target*(qta+add)-(volume+costo))/(volume+costo)
+			hyp_incremento = (target*(qta+add)-(volume+costo))/(volume+costo)
 			hyp_profitto = round((target*(qta+add)-(volume+costo)),2)
-			hyp_decrement = (stop*(qta+add)-(volume+costo))/(volume+costo)
+			hyp_decremento = (stop*(qta+add)-(volume+costo))/(volume+costo)
 			hyp_perdita = round((stop*(qta+add)-(volume+costo)),2)
-			delta_increment = hyp_increment-increment
-			delta_decrement = decrement-hyp_decrement
+			delta_incremento = hyp_incremento-incremento
+			delta_decremento = decremento-hyp_decremento
 			col1, col2, col3 = st.columns(3)
 			with col1:
 				st.write("#### Strategia")
-				st.write("Profitto", f'{increment:.2%}'+" ("+str(profitto)+")")
-				st.write("Perdita ", f'{decrement:.2%}'+" ("+str(perdita)+")")
+				st.write("Profitto", f'{incremento:.2%}'+" ("+str(profitto)+")")
+				st.write("Perdita ", f'{decremento:.2%}'+" ("+str(perdita)+")")
 			with col2:
 				st.write("#### Rimodulazione")
-				st.write("Profitto", f'{hyp_increment:.2%}'+" ("+str(hyp_profitto)+")")
-				st.write("Perdita ", f'{hyp_decrement:.2%}'+" ("+str(hyp_perdita)+")")
+				st.write("Profitto", f'{hyp_incremento:.2%}'+" ("+str(hyp_profitto)+")")
+				st.write("Perdita ", f'{hyp_decremento:.2%}'+" ("+str(hyp_perdita)+")")
 			with col3:
 				st.write("#### Δ")
-				st.write("Profitto", f'{(delta_increment):.2%}')
-				st.write("Perdita ", f'{(delta_decrement):.2%}')
+				st.write("Profitto", f'{(delta_incremento):.2%}')
+				st.write("Perdita ", f'{(delta_decremento):.2%}')
